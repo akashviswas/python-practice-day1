a176193b08a55122fec5f8897df7bbf5ecab858d
akashviswas <akashviswas96@gmail.com>
2026-03-18 00:07:49 +0530

    Day 1:

---
+++ Largest_number.py
@@ -0,0 +1,20 @@
+# Largest numbers
+
+num1 = int(input ("Enter number:>"))
+
+num2 = int(input ("Enter number:>"))
+ 
+num3 = int(input ("Enter number:>"))
+
+if num1 > num2 and num1 > num3: # using (And) and operator we can compair one number to another number....
+      print("Num1 is greater:",num1)
+elif num2 > num3 and num2 > num1:
+        print ("Num2 is greater",num2)
+else: 
+    print ("Num3 is greater",num3)
+
+
+
+
+       
+
