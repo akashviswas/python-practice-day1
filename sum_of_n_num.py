9437e77e94d65258fd42514d6ffd6df740929b7d
akashviswas <akashviswas96@gmail.com>
2026-03-20 03:35:03 +0530

    loops, patterns, and logic building (Day 3)

---
+++ sum_of_n_num.py
@@ -0,0 +1,24 @@
+# Sum of n numbers
+
+
+# loop 
+
+n = int(input("Enter Number:"))
+total = 0 
+
+for i in range ( 1, n+1 ):
+    total +=i
+
+print ("sum is:>",total)
+
+
+
+
+# formula
+
+n = int(input("Enter number: "))
+
+total = n * (n + 1) // 2
+
+print("Sum is:>", total)
+
