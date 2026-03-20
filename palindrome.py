74c5daff43a6b6ee189a324fb5983d49a565c568
akashviswas <akashviswas96@gmail.com>
2026-03-20 23:46:06 +0530

    string operations and logic building (Day 4)

---
+++ palindrome.py
@@ -0,0 +1,19 @@
+# Palindrome check 
+text = input("Enter text: ")
+
+left = 0    
+right = len(text) - 1
+
+is_palindrome = True   
+
+while left < right:
+    if text[left] != text[right]:
+        is_palindrome = False
+        break
+    left += 1
+    right -= 1
+
+if is_palindrome:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
